a6d9894bc13744803b41c11df1584c04884f266d
Andrew D. Yates <andrewyates.name@gmail.com>
2013-05-27 23:38:16 -0400

    fix compute all weaks

--- may26.compute.all.pairs.weak.gse7307.R
+++ may26.compute.all.pairs.weak.gse7307.R
@@ -1,4 +1,4 @@
 source("~/pymod/weak_implication_R/lib.R")
 load("~/brca/E7307.may22.genelvl.exprfilt.RData")
-GSE7307.WEAK <- all.pairs.weak(M)
-save(GSE7307.WEAK, file="~/brca/E7307.may26.err4.th0.1.genelvl.exprfilt.WEAK.RData", err=4, th=0.1)
+GSE7307.WEAK <- all.pairs.weak(M, err=4, th=0.1)
+save(GSE7307.WEAK, file="~/brca/E7307.may26.err4.th0.1.genelvl.exprfilt.WEAK.RData")
--- may26.workbench.R
+++ may26.workbench.R
@@ -27,47 +27,47 @@ H.avg <- as.hclust(Rhclust)
 # This takes forever due to naive implementation
 #Z.complete <- get.compression(CLS.s,H.complete,as.matrix(DCOR.s),min.dcor=0.2)
 
-C.complete.100 <- collapse.cls(CLS.s,cutree(H.complete,100),as.matrix(DCOR.s))
-S.complete.100 <- get.coh.M.score(C.complete.100, 0.2)
-C.avg.100 <- collapse.cls(CLS.s,cutree(H.avg,100),as.matrix(DCOR.s))
-S.avg.100 <- get.coh.M.score(C.avg.100, 0.2)
-S.complete.100$edge.flaws / S.complete.100$edge.all.n #[1] 0.1525253
-S.avg.100$edge.flaws / S.avg.100$edge.all.n           #[1] 0.0979798
+## C.complete.100 <- collapse.cls(CLS.s,cutree(H.complete,100),as.matrix(DCOR.s))
+## S.complete.100 <- get.coh.M.score(C.complete.100, 0.2)
+## C.avg.100 <- collapse.cls(CLS.s,cutree(H.avg,100),as.matrix(DCOR.s))
+## S.avg.100 <- get.coh.M.score(C.avg.100, 0.2)
+## S.complete.100$edge.flaws / S.complete.100$edge.all.n #[1] 0.1525253
+## S.avg.100$edge.flaws / S.avg.100$edge.all.n           #[1] 0.0979798
 
-C.complete.150 <- collapse.cls(CLS.s,cutree(H.complete,150),as.matrix(DCOR.s))
-S.complete.150 <- get.coh.M.score(C.complete.150, 0.2)
-C.avg.150 <- collapse.cls(CLS.s,cutree(H.avg,150),as.matrix(DCOR.s))
-S.avg.150 <- get.coh.M.score(C.avg.150, 0.2)
-S.complete.150$edge.flaws / S.complete.150$edge.all.n #[1] 0.08170022
-S.avg.150$edge.flaws / S.avg.150$edge.all.n           #[1] 0.05852349
+## C.complete.150 <- collapse.cls(CLS.s,cutree(H.complete,150),as.matrix(DCOR.s))
+## S.complete.150 <- get.coh.M.score(C.complete.150, 0.2)
+## C.avg.150 <- collapse.cls(CLS.s,cutree(H.avg,150),as.matrix(DCOR.s))
+## S.avg.150 <- get.coh.M.score(C.avg.150, 0.2)
+## S.complete.150$edge.flaws / S.complete.150$edge.all.n #[1] 0.08170022
+## S.avg.150$edge.flaws / S.avg.150$edge.all.n           #[1] 0.05852349
 
-C.complete.200 <- collapse.cls(CLS.s,cutree(H.complete,200),as.matrix(DCOR.s))
-S.complete.200 <- get.coh.M.score(C.complete.200, 0.2)
-C.avg.200 <- collapse.cls(CLS.s,cutree(H.avg,200),as.matrix(DCOR.s))
-S.avg.200 <- get.coh.M.score(C.avg.200, 0.2)
-S.complete.200$edge.flaws / S.complete.200$edge.all.n #[1] 0.04839196
-S.avg.200$edge.flaws / S.avg.200$edge.all.n           #[1] 0.03562814
-S.avg.200$edge.flaws / S.avg.200$edge.n               #[1] 0.05981608
+## C.complete.200 <- collapse.cls(CLS.s,cutree(H.complete,200),as.matrix(DCOR.s))
+## S.complete.200 <- get.coh.M.score(C.complete.200, 0.2)
+## C.avg.200 <- collapse.cls(CLS.s,cutree(H.avg,200),as.matrix(DCOR.s))
+## S.avg.200 <- get.coh.M.score(C.avg.200, 0.2)
+## S.complete.200$edge.flaws / S.complete.200$edge.all.n #[1] 0.04839196
+## S.avg.200$edge.flaws / S.avg.200$edge.all.n           #[1] 0.03562814
+## S.avg.200$edge.flaws / S.avg.200$edge.n               #[1] 0.05981608
 
-C.complete.250 <- collapse.cls(CLS.s,cutree(H.complete,250),as.matrix(DCOR.s))
-S.complete.250 <- get.coh.M.score(C.complete.250, 0.2)
-C.avg.250 <- collapse.cls(CLS.s,cutree(H.avg,250),as.matrix(DCOR.s))
-S.avg.250 <- get.coh.M.score(C.avg.250, 0.2)
-S.complete.250$edge.flaws / S.complete.250$edge.all.n #[1] 0.03261044
-S.avg.250$edge.flaws / S.avg.250$edge.all.n           #[1] 0.02682731
-S.complete.250$edge.flaws / S.complete.250$edge.n     #[1] 0.05056796
-S.avg.250$edge.flaws / S.avg.250$edge.n               #[1] 0.04427594
-S.avg.250$edge.n / S.avg.250$edge.all.n               #[1] 0.6059116
+## C.complete.250 <- collapse.cls(CLS.s,cutree(H.complete,250),as.matrix(DCOR.s))
+## S.complete.250 <- get.coh.M.score(C.complete.250, 0.2)
+## C.avg.250 <- collapse.cls(CLS.s,cutree(H.avg,250),as.matrix(DCOR.s))
+## S.avg.250 <- get.coh.M.score(C.avg.250, 0.2)
+## S.complete.250$edge.flaws / S.complete.250$edge.all.n #[1] 0.03261044
+## S.avg.250$edge.flaws / S.avg.250$edge.all.n           #[1] 0.02682731
+## S.complete.250$edge.flaws / S.complete.250$edge.n     #[1] 0.05056796
+## S.avg.250$edge.flaws / S.avg.250$edge.n               #[1] 0.04427594
+## S.avg.250$edge.n / S.avg.250$edge.all.n               #[1] 0.6059116
 
-C.avg.225 <- collapse.cls(CLS.s,cutree(H.avg,225),as.matrix(DCOR.s))
-S.avg.225 <- get.coh.M.score(C.avg.225, 0.2)
-S.avg.225$edge.flaws / S.avg.225$edge.n
-#[1] 0.05191221
+## C.avg.225 <- collapse.cls(CLS.s,cutree(H.avg,225),as.matrix(DCOR.s))
+## S.avg.225 <- get.coh.M.score(C.avg.225, 0.2)
+## S.avg.225$edge.flaws / S.avg.225$edge.n
+## #[1] 0.05191221
 
-C.avg.230 <- collapse.cls(CLS.s,cutree(H.avg,230),as.matrix(DCOR.s))
-S.avg.230 <- get.coh.M.score(C.avg.230, 0.2)
-S.avg.230$edge.flaws / S.avg.230$edge.n
-#[1] 0.05106437
+## C.avg.230 <- collapse.cls(CLS.s,cutree(H.avg,230),as.matrix(DCOR.s))
+## S.avg.230 <- get.coh.M.score(C.avg.230, 0.2)
+## S.avg.230$edge.flaws / S.avg.230$edge.n
+## #[1] 0.05106437
 
 # AVERAGE LINKAGE, 5% extant, K=235
 C.avg.235 <- collapse.cls(CLS.s,cutree(H.avg,235),as.matrix(DCOR.s))
@@ -80,19 +80,19 @@ S.avg.235$edge.n / S.avg.235$edge.all.n
 #[1] 0.5995999
 
 
-C.avg.233 <- collapse.cls(CLS.s,cutree(H.avg,233),as.matrix(DCOR.s))
-S.avg.233 <- get.coh.M.score(C.avg.233, 0.2)
-S.avg.233$edge.flaws / S.avg.233$edge.n
-#[1] 0.0504689
-C.avg.232 <- collapse.cls(CLS.s,cutree(H.avg,232),as.matrix(DCOR.s))
-S.avg.232 <- get.coh.M.score(C.avg.232, 0.2)
-S.avg.232$edge.flaws / S.avg.232$edge.n
-#[1] 0.05013029
+## C.avg.233 <- collapse.cls(CLS.s,cutree(H.avg,233),as.matrix(DCOR.s))
+## S.avg.233 <- get.coh.M.score(C.avg.233, 0.2)
+## S.avg.233$edge.flaws / S.avg.233$edge.n
+## #[1] 0.0504689
+## C.avg.232 <- collapse.cls(CLS.s,cutree(H.avg,232),as.matrix(DCOR.s))
+## S.avg.232 <- get.coh.M.score(C.avg.232, 0.2)
+## S.avg.232$edge.flaws / S.avg.232$edge.n
+## #[1] 0.05013029
 
-C.avg.300 <- collapse.cls(CLS.s,cutree(H.avg,300),as.matrix(DCOR.s))
-S.avg.300 <- get.coh.M.score(C.avg.300, 0.2)          # [1] 0.03070961
-S.avg.300$edge.flaws / S.avg.300$edge.all.n           # [1] 0.0187291
-S.avg.300$edge.n / S.avg.300$edge.all.n               # [1] 0.6098774
+## C.avg.300 <- collapse.cls(CLS.s,cutree(H.avg,300),as.matrix(DCOR.s))
+## S.avg.300 <- get.coh.M.score(C.avg.300, 0.2)          # [1] 0.03070961
+## S.avg.300$edge.flaws / S.avg.300$edge.all.n           # [1] 0.0187291
+## S.avg.300$edge.n / S.avg.300$edge.all.n               # [1] 0.6098774
 # ------------------------------
 
 # HOW TO SAVE MEMBER LIST?
@@ -105,7 +105,14 @@ summary(as.factor(WEAK.s))
 
 
 save(C.avg.235, W.avg.235, file="../may26.k235.avglink.5pct.extant.collapsed.RData")
-write.table(C.avg.235$CLS, file="../may26.k235.avglink.5pct.extant.collapsed.CLS.tab", sep="\t", quote=F)
-write.table(C.avg.235$DCOR, file="../may26.k235.avglink.5pct.extant.collapsed.DCOR.tab", sep="\t", quote=F)
-write.table(W.avg.235, file="../may26.k235.avglink.5pct.extant.collapsed.WEAK.tab", sep="\t", quote=F)
-write.table(cutree(H.avg,235), file="../may26.k235.avglink.5ct.extant.collapsed.hclust.csv", sep=",", col.name=F)
+write.table(C.avg.235$CLS, file="../may26.k235.avglink.5pct.extant.collapsed.CLS.tab", sep="\t", col.names=NA)
+write.table(C.avg.235$DCOR, file="../may26.k235.avglink.5pct.extant.collapsed.DCOR.tab", sep="\t", col.names=NA)
+write.table(W.avg.235, file="../may26.k235.avglink.5pct.extant.collapsed.WEAK.tab", sep="\t", col.names=NA)
+write.table(cutree(H.avg,235), file="../may26.k235.avglink.5ct.extant.collapsed.hclust.csv", sep=",", col.names=NA)
+
+pdf("../may26.k235.avglink.5pct.extant.collapsed.GSPLOM.pdf", width=30, height=30)
+R.k235 <- splom(C.avg.235$CLS, C.avg.235$DCOR, grid=F, sym=T)
+dev.off()
+pdf("../may26.k235.avglink.5pct.extant.collapsed.GSPLOM.dendro.pdf", width=30, height=30)
+plot(R.k235$Rhclust)
+dev.off()
