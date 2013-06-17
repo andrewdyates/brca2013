c50ae7ed28352570d302151ed7226efff6c3e76a
Andrew Yates <ayates@master.cl.osumc.edu>
2013-06-17 15:55:46 -0400

    begin to redispatch all pairs bool with more permissive thresholds

---
+++ jun17_dispatch_all_pairs_boolean.sh
@@ -0,0 +1,4 @@
+python ~/code/qsub/script.py script="time python $HOME/code/boolean_implication_fit_py/script.py fname=$HOME/brca/jun6.GSE31448.select.tab b=0.09676083 r_th=0.8" jobname="31448.TF.ALLBOOL2" n_ppn=8 email=True hours=8
+
+python ~/code/qsub/script.py script="time python $HOME/code/boolean_implication_fit_py/script.py fname=$HOME/brca/jun6.GSE7307.select.tab b=0.1541373 r_th=0.9" jobname="7307.TF.ALLBOOL2" n_ppn=8 email=True hours=8
+
